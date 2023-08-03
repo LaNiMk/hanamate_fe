d2509dd4b441c2f9de4c4c7e4bcfc8e630624a53
lake041 <lake041@korea.ac.kr>
2023-08-03 17:21:30 +0900

    0802

--- src/components/InputMoney/InputMoney.js
+++ src/components/InputMoney/InputMoney.js
@@ -31,55 +31,57 @@ const InputMoney = (props) => {
   return (
     <Fragment>
       <Header left="back" title={props.title} right="blank" />
-      <div className={classes.container}>
-        <section className={classes.firstSection}>
-          <h1>{props.message}</h1>
-          <Form method="POST" className={classes.number}>
-            <p>
-              <strong>{amount === "" ? "0" : Number(amount).toLocaleString()}</strong>원
-            </p>
-            <input type="hidden" name="amount" value={Number(amount)} />
-          </Form>
-          {props.subMessage && (
-            <p className={classes.subMessage}>
-              {props.subMessage} {Number(props.balance).toLocaleString()}원
-            </p>
-          )}
-        </section>
-        <section className={classes.secondSection}>
-          <div className={classes.buttonContainer}>
-            <button onClick={() => plusMath(100)}>+ 백원</button>
-            <button onClick={() => plusMath(1000)}>+ 천원</button>
-            <button onClick={() => plusMath(10000)}>+ 만원</button>
-          </div>
-          <div className={classes.numberContainer}>
-            <button onClick={() => addNumber("1")}>1</button>
-            <button onClick={() => addNumber("2")}>2</button>
-            <button onClick={() => addNumber("3")}>3</button>
-            <button onClick={() => addNumber("4")}>4</button>
-            <button onClick={() => addNumber("5")}>5</button>
-            <button onClick={() => addNumber("6")}>6</button>
-            <button onClick={() => addNumber("7")}>7</button>
-            <button onClick={() => addNumber("8")}>8</button>
-            <button onClick={() => addNumber("9")}>9</button>
-            <button onClick={() => addNumber("00")}>00</button>
-            <button onClick={() => addNumber("0")}>0</button>
-            <button onClick={deleteNumber} type="submit">
-              <AiOutlineArrowLeft />
-            </button>
-          </div>
-          <div className={classes.nextContainer}>
-            <SignButton>{props.buttonMessage}</SignButton>
-          </div>
-        </section>
-      </div>
+      <Form method="POST">
+        <div className={classes.container}>
+          <section className={classes.firstSection}>
+            <h1>{props.message}</h1>
+            <div className={classes.number}>
+              <p>
+                <strong>{amount === "" ? "0" : Number(amount).toLocaleString()}</strong>원
+              </p>
+              <input type="hidden" name="amount" value={Number(amount)} />
+            </div>{" "}
+            {props.subMessage && (
+              <p className={classes.subMessage}>
+                {props.subMessage} {Number(props.balance).toLocaleString()}원
+              </p>
+            )}
+          </section>
+          <section className={classes.secondSection}>
+            <div className={classes.buttonContainer}>
+              <button onClick={() => plusMath(100)}>+ 백원</button>
+              <button onClick={() => plusMath(1000)}>+ 천원</button>
+              <button onClick={() => plusMath(10000)}>+ 만원</button>
+            </div>
+            <div className={classes.numberContainer}>
+              <button onClick={() => addNumber("1")}>1</button>
+              <button onClick={() => addNumber("2")}>2</button>
+              <button onClick={() => addNumber("3")}>3</button>
+              <button onClick={() => addNumber("4")}>4</button>
+              <button onClick={() => addNumber("5")}>5</button>
+              <button onClick={() => addNumber("6")}>6</button>
+              <button onClick={() => addNumber("7")}>7</button>
+              <button onClick={() => addNumber("8")}>8</button>
+              <button onClick={() => addNumber("9")}>9</button>
+              <button onClick={() => addNumber("00")}>00</button>
+              <button onClick={() => addNumber("0")}>0</button>
+              <button onClick={deleteNumber} type="submit">
+                <AiOutlineArrowLeft />
+              </button>
+            </div>
+            <div className={classes.nextContainer}>
+              <SignButton>{props.buttonMessage}</SignButton>
+            </div>
+          </section>
+        </div>
+      </Form>
     </Fragment>
   );
 };
 
 export default InputMoney;
 
-export async function action({ request, params }) {
+export async function action({ request }) {
   const method = request.method;
   const data = await request.formData();
   const requestData = {
@@ -94,6 +96,7 @@ export async function action({ request, params }) {
     },
     body: JSON.stringify(requestData),
   });
+  console.log(response);
 
   console.log(response.data.Status);
 
